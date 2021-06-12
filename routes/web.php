23169bd76bd17c326e49a1b856c175b0ce2f2655
YISivlay <sivlayy91@gmail.com>
2021-06-12 12:07:43 +0700

    Create Checklist Group and Checklist management.

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+/*This middleware group is for admin page*/
 Route::group(['middleware' => 'auth'], function (){
    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function (){
       Route::resource('pages',\App\Http\Controllers\Admin\PageController::class);
+      Route::resource('checklist_groups',\App\Http\Controllers\Admin\ChecklistGroupController::class);
+      Route::resource('checklist_groups.checklists',\App\Http\Controllers\Admin\ChecklistController::class);
    });
 });
+/*End*/
