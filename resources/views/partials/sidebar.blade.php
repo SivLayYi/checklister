da1407430a03951451e8db6a2d8a743b2c1b8cca
YISivlay <sivlayy91@gmail.com>
2021-06-15 15:43:50 +0700

    User's checklist and css fixes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,21 +12,26 @@
                 <li class="c-sidebar-nav-item c-sidebar-nav-dropdown c-show">
                     <a class="c-sidebar-nav-link" href="{{route('admin.checklist_groups.edit',$group->id)}}">
                         <svg class="c-sidebar-nav-icon">
-                            <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle')}}"></use>
+                            <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-folder-open')}}"></use>
                         </svg>
                         {{$group->name}}
                     </a>
                     <ul class="c-sidebar-nav-dropdown-items">
                         @foreach($group->checklists as $checklist)
                         <li class="c-sidebar-nav-item">
-                            <a class="c-sidebar-nav-link" href="{{route('admin.checklist_groups.checklists.edit',[$group, $checklist])}}">
-                                <span class="c-sidebar-nav-icon"></span>
+                            <a class="c-sidebar-nav-link" style="padding: .5rem .5rem .5rem 76px" href="{{route('admin.checklist_groups.checklists.edit',[$group, $checklist])}}">
+                                <svg class="c-sidebar-nav-icon">
+                                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-list')}}"></use>
+                                </svg>
                                 {{$checklist->name}}
                             </a>
                         </li>
                         @endforeach
                         <li  class="c-sidebar-nav-item">
                             <a class="c-sidebar-nav-link" href="{{route('admin.checklist_groups.checklists.create',$group)}}">
+                                <svg class="c-sidebar-nav-icon">
+                                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-note-add')}}"></use>
+                                </svg>
                                 {{__('New checklist')}}
                             </a>
                         </li>
@@ -35,6 +40,9 @@
             @endforeach
             <li  class="c-sidebar-nav-item c-sidebar-nav-dropdown">
                 <a class="c-sidebar-nav-link" href="{{route('admin.checklist_groups.create')}}">
+                    <svg class="c-sidebar-nav-icon">
+                        <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-library-add')}}"></use>
+                    </svg>
                     {{__('New checklist group')}}
                 </a>
             </li>
@@ -60,6 +68,20 @@
                     {{__('Users')}}
                 </a>
             </li>
+        @else
+            @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $group)
+                <li class="c-sidebar-nav-title">{{ $group->name }}</li>
+                @foreach($group->checklists as $checklist)
+                    <li class="c-sidebar-nav-item">
+                        <a class="c-sidebar-nav-link" href="{{route('user.checklists.show',[$checklist])}}">
+                            <svg class="c-sidebar-nav-icon">
+                                <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-list')}}"></use>
+                            </svg>
+                            {{$checklist->name}}
+                        </a>
+                    </li>
+                @endforeach
+            @endforeach
         @endif
         {{--End--}}
     </ul>
---
+++ resources/views/users/checklists/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        {{ $checklist->name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#task-textarea' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+@endsection
