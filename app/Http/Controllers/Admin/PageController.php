053852faf7b4749e50d6c8a04cf8a73b8b5106bb
YISivlay <sivlayy91@gmail.com>
2021-06-15 10:06:10 +0700

    Add stubs publish to project

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -3,84 +3,47 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class PageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return string
-     */
-    public function index()
+    public function index() : View
     {
-        return 'Success page!';
+        $pages = Page::all();
+
+        return view('pages.index', compact('pages'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
+    public function create() : View
     {
-        return null;
+        return view('pages.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
-        return null;
+        return redirect()->route('pages.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
+    public function show(Page $page) : View
     {
-        return null;
+        return view('pages.show', compact('page'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
+    public function edit(Page $page) : View
     {
-        return null;
+        return view('pages.edit', compact('page'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, Page $page) : RedirectResponse
     {
-        return null;
+        return redirect()->route('pages.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
+    public function destroy(Page $page) : RedirectResponse
     {
-        return null;
+        return redirect()->route('pages.index');
     }
 }
