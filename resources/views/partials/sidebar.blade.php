230a854b509dd1d6a1c3e015a28288b8eb129e7b
YISivlay <sivlayy91@gmail.com>
2021-06-15 12:19:56 +0700

    Welcome page and top-right munu links

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,15 +4,6 @@
 </style>
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
     <ul class="c-sidebar-nav">
-        <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="{{ route('home') }}">
-                <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer')}}"></use>
-                </svg>
-                {{__('Dashboard')}}
-            </a>
-        </li>
-
         {{--Dispaly page with admin user--}}
         @if(auth()->user()->is_admin)
             <li class="c-sidebar-nav-title">{{__('Manage Checklists')}}</li>{{--We use this for support multiple languages--}}
