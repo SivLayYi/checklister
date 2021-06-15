129bbd79107708fe04e3e7936f8d828f3101a9e5
YISivlay <sivlayy91@gmail.com>
2021-06-15 09:46:36 +0700

    Exclude Docblock on top of method, using with return hinttype

--- app/Http/Controllers/Admin/ChecklistController.php
+++ app/Http/Controllers/Admin/ChecklistController.php
@@ -6,73 +6,35 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreChecklistRequest;
 use App\Models\Checklist;
 use App\Models\ChecklistGroup;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Response;
 
 class ChecklistController extends Controller
 {
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @param ChecklistGroup $checklistGroup
-     * @return Application|Factory|View|Response
-     */
-    public function create(ChecklistGroup $checklistGroup)
+    public function create(ChecklistGroup $checklistGroup) : View
     {
         return view('admin.checklists.create', compact('checklistGroup'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param StoreChecklistRequest $request
-     * @param ChecklistGroup $checklistGroup
-     * @return RedirectResponse
-     */
-    public function store(StoreChecklistRequest $request, ChecklistGroup $checklistGroup)
+    public function store(StoreChecklistRequest $request, ChecklistGroup $checklistGroup) : RedirectResponse
     {
         $checklistGroup->checklists()->create($request->validated());
 
         return redirect()->route('home');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param ChecklistGroup $checklistGroup
-     * @param Checklist $checklist
-     * @return Application|Factory|View|void
-     */
-    public function edit(ChecklistGroup $checklistGroup, Checklist $checklist)
+    public function edit(ChecklistGroup $checklistGroup, Checklist $checklist) : View
     {
         return view('admin.checklists.edit', compact('checklistGroup', 'checklist'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param StoreChecklistRequest $request
-     * @param ChecklistGroup $checklistGroup
-     * @param Checklist $checklist
-     * @return RedirectResponse
-     */
-    public function update(StoreChecklistRequest $request, ChecklistGroup $checklistGroup, Checklist $checklist)
+    public function update(StoreChecklistRequest $request, ChecklistGroup $checklistGroup, Checklist $checklist) : RedirectResponse
     {
         $checklist->update($request->validated());
         return redirect()->route('home');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param ChecklistGroup $checklistGroup
-     * @param Checklist $checklist
-     * @return RedirectResponse
-     */
-    public function destroy(ChecklistGroup $checklistGroup, Checklist $checklist)
+    public function destroy(ChecklistGroup $checklistGroup, Checklist $checklist) : RedirectResponse
     {
         $checklist->delete();
         return redirect()->route('home');
